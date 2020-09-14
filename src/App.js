a9ae93d7ab3b630f43ae96a41b600d0c2d80a320
sashitaghimire <ghimiresashita@gmail.com>
2020-09-14 14:17:37 +0545

    build rows

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Netflix clone</h1>
-      <Row  title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrginals}/>
+      {/* Nav bar */}
+      {/* Banner */}
+      <Row  title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,9 @@ import React,{useState,useEffect} from 'react';
 import axios from './axios';
 import './Row.css';
 
-function Row({title, fetchUrl}) {
+const base_url ="https://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(()=>{
@@ -21,6 +23,15 @@ function Row({title, fetchUrl}) {
     return (
         <div className="row">
             <h2>{title}</h2>
+            <div className="row__posters">
+                {/* serveral row posters */}
+
+                {movies.map(movie=>(
+                    <img key={movie.id}
+                     className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path :movie.backdrop_path}`} alt={movie.name} />
+                ))}
+
+            </div>
             {/* title */}
             {/* container -> posters */}
         </div>
